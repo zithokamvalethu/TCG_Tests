17cd5c205b05c99f72189bc3f73da84a05c1b295
Selfmade20 <ratshikombofarisani@gmail.com>
2021-09-28 10:39:42 +0200

    added a kata file for removing an item

---
+++ removeItem.js
@@ -0,0 +1,23 @@
+var list = ["kamva", "roman", "amanda", "zizo"];
+const removeItem=(list,names)=> {
+  return list.filter((item) => item !==names) ;
+};
+console.log(removeItem(list,"zizo"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
