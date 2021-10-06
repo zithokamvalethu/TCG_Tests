8876a7baddbba4f7375353a3dee851c9d52ff173
Selfmade20 <ratshikombofarisani@gmail.com>
2021-10-06 13:06:12 +0200

    added new kata solution file

---
+++ multiplyAndReturnOdd.js
@@ -0,0 +1,56 @@
+// const multiplyAndReturnOdd =[1,2,3,4,6]
+// if ([] == [4, 6]){
+//   return [1, 2, 3] * 3
+// } else if ([] == ![1, 2, 3])
+//   return []
+// } else {
+//   return 0
+//   return a * b;
+
+// }
+// console.log(1* 3);
+
+const MultiplyAndReturnOdd = [1, 2, 3, 4, 5, 7];
+const multiplyAndReturnOdd = (num) => {
+  const multipliedNumber = [];
+  const oddValues = [];
+  for (var i in num) {
+    multipliedum.push(num[i] * 3);
+  }
+  console.log(multipliedNumber);
+  for (var i in multipliedNumber) {
+    if (multipliedNumber[i] % 2 !== 0) {
+      oddValues.push(multipliedNumber[i]);
+    }
+  }
+
+  return oddValues;
+};
+console.log(MultiplyAndReturnOdd(num));
+// const list = [1, 2, 3];
+// const multiplyAndGetOdd = (list) => {
+//   const multipliedList = []
+//   const oddValues = [];
+//   for(var i in list){
+//       multipliedList.push(list[i] * 3)
+//   }
+//
+//   console.log(multipliedList)
+
+//   for (var i in multipliedList) {
+//     if (multipliedList[i] % 2 !== 0) {
+//       oddValues.push(multipliedList[i]);
+//     }
+//   }
+
+//   return oddValues;
+// };
+
+// console.log(multiplyAndGetOdd(list));
+// if ([] !== % 3) {
+//     return  [] * 3
+//   } else if ([] ==! % 2) {
+//     return [];
+//   } else {
+//     return [];
+//   }
